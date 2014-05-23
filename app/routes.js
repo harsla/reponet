b14078a168bcc82aa1a8decd44d6210e8d75b6b6
Scott Harsla <harsla@gmail.com>
2014-05-22 21:56:20 -0700

    Inital Commit

---
+++ app/routes.js
@@ -0,0 +1,92 @@
+module.exports = function(app, passport) {
+
+  // homepage
+	app.get('/', function(req, res) {
+		res.render('index.ejs'); // load the index.ejs file
+	});
+
+	 //login page
+   app.get('/login', function(req, res) {
+	 	res.render('login.ejs', { message: req.flash('loginMessage') });
+   });
+
+  	// process the login form
+	app.post('/login', passport.authenticate('local-login', {
+		//successRedirect : '/profile', // redirect to the secure profile section
+    successRedirect : '/dashboard', // redirect to the secure profile section
+		failureRedirect : '/login', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+
+  //signup page
+	app.get('/signup', function(req, res) {
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
+
+  // process the signup form
+	app.post('/signup', passport.authenticate('local-signup', {
+		//successRedirect : '/profile', // redirect to the secure profile section
+    successRedirect : '/dashboard', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+
+  //profile (req. auth)
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile.ejs', {
+			user : req.user
+		});
+	});
+
+  //dashboard (req. auth)
+  app.get('/dashboard', isLoggedIn, function(req, res) {
+    res.render('dashboard.ejs', {
+      user : req.user
+    });
+  });
+
+  //order (req. auth)
+  app.get('/order', isLoggedIn, function(req, res) {
+    res.render('order.ejs', {
+      user : req.user
+    });
+  });
+
+  //order (req. auth)
+  app.get('/reports', isLoggedIn, function(req, res) {
+    res.render('reports.ejs', {
+      user : req.user
+    });
+  });
+
+  //order (req. auth)
+  app.get('/inventory', isLoggedIn, function(req, res) {
+    res.render('inventory.ejs', {
+      user : req.user
+    });
+  });
+
+  //order (req. auth)
+  app.get('/zones', isLoggedIn, function(req, res) {
+    res.render('zones.ejs', {
+      user : req.user
+    });
+  });
+
+  //logout
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 8080,
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    flash = require('connect-flash');
+
+var configDB = require('./config/database.js');
+
+// connect to the DB
+mongoose.connect(configDB.url);
+require ('./config/passport')(passport);
+
+app.configure(function() {
+  // express
+
+  app.use(express.logger('dev'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+  // templating
+  app.set('view engine', 'ejs');
+
+  // passport
+  app.use(express.session({secret: 'rinchanrinchanrinchan'}));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(flash());
+
+});
+
+// give routes the app and passport
+require('./app/routes.js')(app, passport);
+
+// start the app
+app.listen(port);
+console.log('app running on port ' + port);
